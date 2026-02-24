ac12d3162313648089f4ef25e01332b2f350c51a
Elsa Girardo <egirardo@hotmail.com>
2026-02-24 12:40:07 +0100

    Created and migrated databases

---
+++ mtg-storefront/database/migrations/2026_02_24_113208_create_singles_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('singles_products', function (Blueprint $table) {
+            $table->unsignedInteger('product_id')->primary();
+            $table->string('rarity', 50)->nullable();
+            $table->string('color', 50)->nullable();
+            $table->string('number', 50)->nullable();
+            $table->string('set_name', 50)->nullable();
+            $table->foreign('product_id')->references('product_id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('singles_products');
+    }
+};

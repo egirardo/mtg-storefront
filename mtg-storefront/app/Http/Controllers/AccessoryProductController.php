496e2d11dfdf58a4f21f07a7ba8a308b34a494b9
Elsa Girardo <egirardo@hotmail.com>
2026-02-24 14:05:48 +0100

    Created controllers, models, factories, and seeders for Product, Category, SealedProduct, SingleProduct, and Accessory Product

---
+++ mtg-storefront/app/Http/Controllers/AccessoryProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessoryProduct;
+use Illuminate\Http\Request;
+
+class AccessoryProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AccessoryProduct $accessoryProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AccessoryProduct $accessoryProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AccessoryProduct $accessoryProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AccessoryProduct $accessoryProduct)
+    {
+        //
+    }
+}
--- mtg-storefront/app/Models/SealedProduct.php
+++ mtg-storefront/app/Models/SealedProduct.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SealedProduct extends Product
 {
-    //
+    /** @use HasFactory<\Database\Factories\SealedProductFactory> */
+    use HasFactory;
 }

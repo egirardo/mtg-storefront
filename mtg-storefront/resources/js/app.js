a0cf512d3da9513221643eb04f49bce094afded1
mariatedeman <m.tedeman@hotmail.com>
2026-03-03 13:44:52 +0100

    sorting function added to products table

--- mtg-storefront/resources/js/app.js
+++ mtg-storefront/resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+// Sorting / filter function for products table
+document.addEventListener("DOMContentLoaded", function () {
+    const select = document.getElementById("sorting");
+
+    if(select) {
+        select.addEventListener("change", function () {
+            this.form.submit();
+        });
+    }
+});
